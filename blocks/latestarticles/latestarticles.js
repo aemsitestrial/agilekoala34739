9160a45abaa2c27211abc2e9c7674331973bed6f
Naveenkumar S <2963078@tcs.com>
2026-09-16 12:23:20 +0530

    build erros fixed

--- blocks/latestarticles/latestarticles.js
+++ blocks/latestarticles/latestarticles.js
@@ -1,28 +1,28 @@
 export default function decorate(block) {
-    const ul = document.createElement('ul');
-  
-    [...block.children].forEach((row) => {
-      const li = document.createElement('li');
-  
-      const [label, category, title, link] = [...row.children];
-  
-      li.innerHTML = `
-        ${link?.textContent || '#'}
-          <div class="article-meta">
-            <span class="article-label">${label?.textContent || ''}</span>
-            <span class="article-category">${category?.textContent || ''}</span>
-          </div>
-  
-          <div class="article-content">
-            <h3>${title?.textContent || ''}</h3>
-            <span class="article-arrow">→</span>
-          </div>
-        </a>
-      `;
-  
-      ul.append(li);
-    });
-  
-    block.textContent = '';
-    block.append(ul);
-  }
+  const ul = document.createElement('ul');
+
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+
+    const [label, category, title, link] = [...row.children];
+
+    const articleLink = link?.textContent?.trim() || '#';
+
+    li.innerHTML = `
+      <a href="${articleLink}" class="articlecle-meta">
+          <span class="article-label">${label?.textContent || ''}</span>
+          <span class="article-category">${category?.textContent || ''}</span>
+        </div>
+        <div class="article-content">
+          <h3>${title?.textContent || ''}</h3>
+          <span class="article-arrow">→</span>
+        </div>
+      </a>
+    `;
+
+    ul.append(li);
+  });
+
+  block.textContent = '';
+  block.append(ul);
+}
